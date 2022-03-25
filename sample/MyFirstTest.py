422a9bcb4fda793b1b99dbee0de3aee15ba30d15
Mukesh Otwani <mukeshotwani.50@gmail.com>
2022-03-25 17:56:59 +0530

    changes done

--- sample/MyFirstTest.py
+++ sample/MyFirstTest.py
@@ -5,5 +5,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 def test_myLogin():
     driver=webdriver.Chrome(ChromeDriverManager().install())
     driver.get("http://www.google.com")
+    print("Page Title is "+driver.title)
+    print("Page url is "+driver.current_url)
+    assert driver.title == "Google"
     driver.find_element(By.NAME,"q").send_keys("Mukesh Otwani")
     driver.quit()
